343c490168c9479665793fcc1c8447bea57ac12f
Ross Terker <ross.terker@gmail.com>
2024-10-22 21:36:19 -0400

    generating css selector for clicked node. now need to store it and work on playback

--- content-script.js
+++ content-script.js
@@ -33,17 +33,17 @@ const listeners = [
 
             const tabInfoResponse = await chrome.runtime.sendMessage('get-tab-info');
             
-            console.log('\n');
-            console.log(`content-script.js: mousemove tab info:`);
-            console.log(`tabId: ${tabInfoResponse.tabId}`);
-            console.log(`tabUrl: ${tabInfoResponse.tabUrl}`);
+            // console.log('\n');
+            // console.log(`content-script.js: mousemove tab info:`);
+            // console.log(`tabId: ${tabInfoResponse.tabId}`);
+            // console.log(`tabUrl: ${tabInfoResponse.tabUrl}`);
             
             const mouseMoveResponse = await chrome.runtime.sendMessage({ action: 'mousemove', tabId: tabInfoResponse.tabId, tabUrl: tabInfoResponse.tabUrl, x, y, time });
             ({ x, y, time } = mouseMoveResponse);
-            console.log(`Mouse moved! Values stored in background storage => tabId: ${mouseMoveResponse.tabId}, tabUrl: ${mouseMoveResponse.tabUrl}, x: ${x}, y: ${y}, time: ${time}`);
+            // console.log(`Mouse moved! Values stored in background storage => tabId: ${mouseMoveResponse.tabId}, tabUrl: ${mouseMoveResponse.tabUrl}, x: ${x}, y: ${y}, time: ${time}`);
 
             let endTime = Date.now();
-            console.log(`Round trip operation from mouse move to storage to response received back in content script: ${endTime - time} ms`);
+            // console.log(`Round trip operation from mouse move to storage to response received back in content script: ${endTime - time} ms`);
         }
     },
     {
@@ -57,8 +57,8 @@ const listeners = [
 
             console.log('\n');
             console.log(`In content-script.js, click target is ${target}`);
-            console.log(`Target nodeName is ${target.nodeName}`);
-            console.log(`Target nodeType is ${target.nodeType}`);
+            const cssSelector = generateCssSelector(target);
+            console.log('cssSelector for clicked target:', cssSelector)
             
             const tabInfoResponse = await chrome.runtime.sendMessage('get-tab-info');
             
@@ -103,4 +103,55 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         sendResponse({ tabId: message.tabId, message: 'content-script-recording-stopped'});
     }
 });
+
+
+function generateCssSelector(target) {
+    const path = [];
+    // first check if the element has an id, if it does, use #elementId,
+    // if (target.id) {
+    //         return `#${target.id}`;
+    // }
+    // if not: find if there are any siblings of same type:
+    let current = target;
+    while (current !== null) {
+        const currTagName = current.tagName.toLowerCase();
+        const parent = current.parentElement;
+        //reach html element and parent is null
+        if (parent === null) {
+            path.unshift(currTagName);
+            break;
+        }
+
+        const siblings = parent.children;
+        if (siblings.length === 1) {
+            path.unshift(currTagName);
+            current = parent;
+            continue;
+        }
+
+        let totalCount = 0;
+        let currCount;
+        for (let sibling of siblings) {
+            if (current === sibling) {
+                totalCount++;
+                currCount = totalCount;
+                continue;
+            }
+            const siblingTagName = sibling.tagName.toLowerCase();
+            if (siblingTagName === currTagName) {
+                totalCount++;
+            }
+        }
+
+        if (totalCount === 1) {
+            path.unshift(currTagName);
+        } else {
+            path.unshift(`${currTagName}:nth-of-type(${currCount})`);
+        }
+
+        current = parent;
+    }
+
+    return path.join(' > ');
+}
 
